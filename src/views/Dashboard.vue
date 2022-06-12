df63502e5be66dfa362eb76ed7d0ea8f19b7b645
bryanballidev <bryanballidev@gmail.com>
2022-06-12 18:05:43 +0300

    wallet_udpate

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="bg-blue-75">
+    <div class="text-4xl md:w-full">Become a validator and help secure the future of Ethereum.</div>
+    <div>Earn continous reward for providing a public good to the community.</div>
+    <button class="wallet-btn border border-black p-2">CONNECT WALLET</button>
+    <img src="" alt="">
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          75: '#E1F3FA',
+        },
+        "pink": "#FDCCD3",
+        "btn-blue": "#FDB9BA",
+      },
+    },
   },
   plugins: [],
 }
