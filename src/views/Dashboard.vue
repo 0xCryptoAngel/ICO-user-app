9dfc286023fcf7f35a942fd0dc7f2611fdc2c2cc
bryanballidev <bryanballidev@gmail.com>
2022-06-12 19:59:48 +0300

    udpate

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -269,5 +269,37 @@
     </div>
 
     <!-- Validators -->
+    <div>
+      <img
+        class="w-12 h-36 my-8 ml-4"
+        src="@/assets/eth-diamond-plain.svg"
+        alt="diamond"
+      />
+      <div>
+        <div>Validators and Ethereum</div>
+        <div>
+          This launchpad will help you become a validator, so you can play an active part in Ethereum’s future. Validators are key to the more secure, scalable, and sustainable Ethereum we’re building together.
+        </div>
+        <a href="#">
+          Formal verification report ↗
+        </a>  
+        <div>
+          As a validator, you’ll be responsible for securing the network and receive continuous payouts for actions that help the network reach consensus.
+        </div>
+        <a href="#">
+          More on the Beacon Chain ↗
+        </a>
+        <a href="#">
+          More on the merge ↗
+        </a>
+        <div>
+          Today, you’ll secure the Beacon Chain, the first main scaling upgrade. It’s a separate chain that uses a proof-of-stake consensus mechanism. Eventually you’ll help secure all of Ethereum, once mainnet (the Ethereum we use today) merges with the Beacon Chain.
+          Validating in Ethereum is not the same as mining. The outcomes are similar: the work you do will extend and secure the chain. But the process is completely different because they use different consensus mechanisms.
+        </div>
+        <a href="#">
+          More on consensus mechanisms ↗
+        </a>
+      </div>
+    </div>
   </div>
 </template>
