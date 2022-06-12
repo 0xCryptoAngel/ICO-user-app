51a862116ec0ef7d51aa7b147a8370f9097b1d44
bryanballidev <bryanballidev@gmail.com>
2022-06-12 12:58:47 +0300

    initial_configuration_complete

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from '@/store'
-import router from './router/router'
+import router from '@/router'
 import './index.css'
 
 createApp(App)
